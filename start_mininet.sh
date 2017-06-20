05499f5a4e43220d593a8f5491ce2508f456bd95
Damien Saucez <damien.saucez@inria.fr>
2017-06-20 18:53:46 +0200

    sanity on config and log directories

--- start_mininet.sh
+++ start_mininet.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+if [ -d config ]
+then
+   sudo rm -rf config
+fi
+mkdir config
+
+if [ -d log ]
+then
+  sudo rm -rf log
+fi
+mkdir log
+
 python myController.py &
 controller=$!
 sudo ./cleanup2.sh && sudo ./run_demo2.sh
